69c5897669cc2fd2c14779c807585263f0de9cfe
Biba <56616264+sigovdlinde@users.noreply.github.com>
2024-01-10 18:22:48 +0100

    remove comments, make everything ready for nig. Add statistics layout

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def get_latest_trout_number(api_url):
     response = requests.get(api_url)
     if response.status_code == 200:
         data = response.json()
-        # Assuming the latest trout number is in the 'id' field of the last trout in the list
         latest_trout_number = data['result'][-1]['id']
         return latest_trout_number
     else:
@@ -57,9 +56,7 @@ def download_and_convert_trout_images(directory='./static/trouts'):
     for trout_number in range(1, latest_trout_number + 1):
         trout_str = str(trout_number)
         
-        # Skip downloading if the image already exists
         if trout_str in existing_images:
-            print(f"Image for trout #{trout_number} already exists.")
             continue
         
         url = generate_trout_image_url(trout_number)
@@ -72,11 +69,9 @@ def download_and_convert_trout_images(directory='./static/trouts'):
             with open(svg_file_path, 'wb') as file:
                 file.write(response.content)
             
-            # Convert SVG to PNG
             cairosvg.svg2png(url=svg_file_path, write_to=png_file_path)
             print(trout_number)
             
-            # Remove the SVG file if you don't need it to save space
             os.remove(svg_file_path)
         else:
             print(f"Failed to download image for trout #{trout_number}")
@@ -102,10 +97,8 @@ def build_family_tree(data, trout_id, tree=None, level=0):
     if trout_info is None:
         return None
 
-    # Here, trout_info[0] should be the 'id' of the trout
     tree[trout_info[0]] = {'id': trout_info[0], 'coi': trout_info[1], 'level': level, 'children': {}}
 
-    # Now, do the same for the left and right parents if they exist
     left_parent_id = trout_info[2]
     right_parent_id = trout_info[3]
     if left_parent_id:
@@ -166,14 +159,11 @@ def get_color(inbreeding_coefficient):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     api_data = get_api_data()
-    if api_data is None:
-        return "Error fetching data from API", 500
-
     processed_data = process_api_data(api_data)
 
     trout_id = None
     family_tree_image = None
-    # Set a default value for tree_type in case it's not in the form data
+
     tree_type = request.form.get('tree_type', 'full_tree')
     if request.method == 'POST':
         trout_id = request.form.get('trout_id')
@@ -184,15 +174,12 @@ def index():
 
         # Depending on the type of tree requested, build the appropriate tree
         if tree_type == 'ancestors':
-            # Call your existing function to build the family tree
             family_tree = build_family_tree(processed_data, int(trout_id))
             add_nodes_edges(G, family_tree[int(trout_id)])
         elif tree_type == 'descendants':
-            # Build the full descendant tree for the given trout
             descendant_tree = build_full_descendant_tree(processed_data, int(trout_id))
             add_descendants_to_graph(G, int(trout_id), descendant_tree)
         elif tree_type == 'full_tree':
-            # Combine both ancestors and descendants into the full tree
             family_tree = build_family_tree(processed_data, int(trout_id))
             add_nodes_edges(G, family_tree[int(trout_id)])
             descendant_tree = build_full_descendant_tree(processed_data, int(trout_id))
@@ -200,9 +187,9 @@ def index():
 
         if show_images:
 
+            # Update trout images.
             download_and_convert_trout_images()
 
-            # Draw the graph
             node_count = len(G.nodes)
             if node_count < 15:
                 figsize = (20, 20)
@@ -221,8 +208,7 @@ def index():
                 image_path = f'./static/trouts/trout_{node}.png'
                 img = Image.open(image_path)
                 
-                # Do not resize to thumbnail, use the original size, or resize to a size that maintains quality
-                desired_size = (size_x, size_y)  # Adjust as needed based on the original image size
+                desired_size = (size_x, size_y)
                 img = img.resize(desired_size, Image.Resampling.LANCZOS)
                 
                 xi, yi = pos[node]
@@ -231,29 +217,20 @@ def index():
                 zoom_factor = desired_size[0] / img.size[0]
                 xi, yi = pos[node]
                 rgba_color = get_color(G.nodes[node].get('inbreeding', 0))
-                border_size = 100
-                border_rect = Rectangle((xi - border_size / 2, yi - border_size / 3),
-                                        border_size, border_size / 3, linewidth=2,
-                                        edgecolor=rgba_color, facecolor='none', zorder=0)
-                # ax.add_patch(border_rect)
+
                 im = OffsetImage(img, zoom=zoom_factor)
                 ab = AnnotationBbox(im, (xi, yi), xycoords='data', frameon=False, zorder=1)
                 ax.add_artist(ab)
                 ax.text(xi, yi + 8, f'#{node}', ha='center', va='bottom', zorder=2, color='black', fontsize=20)
             
             plt.axis('off')
-            # Save the plot to a file
             plot_filename = 'family_tree.png'
             plt.savefig(f'./static/{plot_filename}', dpi=300)
             plt.close()
-            # Pass the filename of the plot to the template
             return render_template('index.html', family_tree_image=url_for('static', filename=plot_filename))
         else:
-            # Use the Graphviz layout to position the nodes
             pos = graphviz_layout(G, prog='dot', args='-Grankdir=TB')
-            # Get node colors based on the inbreeding coefficient
             node_colors = [get_color(G.nodes[node].get('inbreeding', 0)) for node in G.nodes()]
-            # Draw the graph
             node_count = len(G.nodes)
 
             if node_count < 30:
@@ -270,8 +247,14 @@ def index():
             plt.close()
             return render_template('index.html', family_tree_image='static/family_tree.png')
 
-    # Initial or non-POST request
     return render_template('index.html', family_tree_image=None, data=processed_data)
+
+# START WORKING HERE.
+@app.route('/statistics')
+def statistics():
+    api_data = get_api_data()
+    processed_data = process_api_data(api_data)
+    return render_template('statistics.html')
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=False)
